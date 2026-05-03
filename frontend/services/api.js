a17057c0797e3a7d27bf033fce2a6b2c17b2538d
Dashitha <sansaladashitha287@gmail.com>
2026-05-03 15:01:34 +0530

    wmt frontend

--- frontend/services/api.js
+++ frontend/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const BASE_URL = 'http://192.168.8.160:5000/api';
+const BASE_URL = 'https://wmt-mobile-application-production.up.railway.app/api';
 
 const api = axios.create({ baseURL: BASE_URL });
 
